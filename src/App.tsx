6cc2fe282d0f3e77a199d875af8cf53ec5760d3e
Hemant Kakkar <hemant@administrators-MacBook-Pro.local>
2024-02-29 12:30:37 +0530

    Adding timers using context API and adding useReducer

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,18 @@
-import { useRef } from "react";
-import Button from "./components/Button";
-import Container from "./components/Container";
-import Input from "./components/Input";
-import Form, {FormHandle} from "./components/Form";
+import AddTimer from './components/AddTimer.tsx';
+import Header from './components/Header.tsx';
+import Timers from './components/Timers.tsx';
+import TimersContextProvider from './store/timers-context.tsx';
 
 function App() {
-  const customForm = useRef<FormHandle>(null)
-
-  function handleSave(data: unknown) {
-    if(!data || typeof data !== 'object' || !('name' in data) || !('age' in data)) {
-      return;
-    }
-    // const extractedData = data as {name: string, age: string}
-    console.log(data)
-    customForm.current?.clear()
-  }
-
-  return (<main>
-    <Form onSave={handleSave} ref={customForm}>
-      <Input id="name" label = "Your name" type="text"/>
-      <Input id="age" label = "Your age" type="number"/>
-      <p><Button>Save</Button></p>
-    </Form>
-      <p><Button href="https://google.com"> A link</Button></p>
-      <Container as={Button}>click me</Container>
-    </main>)
+  return (
+    <TimersContextProvider>
+      <Header />
+      <main>
+        <AddTimer />
+        <Timers />
+      </main>
+    </TimersContextProvider>
+  );
 }
 
 export default App;
